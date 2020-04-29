06d3a845ab3b13c33139ec4f8c3997e3a7683dc5
jinglixia_v <jing1763112943@163.com>
2020-04-29 18:01:54 +0800

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,53 @@
-const { series, parallel, src, dest } = require('gulp')
+const { series, parallel, src, dest, lastRun, watch } = require('gulp')
 const uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 var del = require('del');
+var preprocess = require('gulp-preprocess')
 
 const projectname = require('./package.json').name
 const buildfile = '.build'
 const PROJECT_DIR = projectname + '/' + buildfile
 
+//环境变量
+const env = process.env.NODE_ENV
+let target = env === 'production' ? './dist' : './pre'
+console.log('当前环境：'+env+'对应打包地址：'+target)
+
 function delFiles(cb) {
-  console.log('删除gulp_test文件')
   del(projectname)
   cb()
 }
 
-exports.buildModule = series(delFiles, streamTaskHtml, uglifyCss, uglifyJs)
-
 function streamTaskHtml() {
   return src('src/*.html')
+    .pipe(preprocess({
+        context: {
+        // 此处可接受来自调用命令的 NODE_ENV 参数，默认为 development 开发测试环境
+          NODE_ENV: process.env.NODE_ENV || 'development',
+        },
+    })) 
     .pipe(htmlmin())
     .pipe(dest(PROJECT_DIR + '/html/'));
 }
 exports.streamTaskHtml = streamTaskHtml;
 
-function streamTaskImg() {
-  return src(['src/img/*'])
+function images() {
+  // return src('src/img/*', { since: lastRun(images) })
+  return src('src/img/*.jpg', { since: lastRun(images) })
+    .pipe(imagemin({
+      optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+      progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+      interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+      multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
+      svgoPlugins: [{
+          removeViewBox: false
+      }],
+  }))
     .pipe(dest(PROJECT_DIR + '/img/'));
 }
-exports.streamTaskImg = streamTaskImg;
+exports.images = images
 
 function uglifyCss() {
   return src('src/css/*.css')
@@ -57,52 +77,56 @@ function script() {
       .pipe('dist/*')
 }
 exports.script = script
-// ------------------------- 分割线 -------------------------
 
-function defaultTask(cb) {
-  console.log(`新版gulp4.0.0的语法`)
-  cb();
-}
+exports.buildModule = series(delFiles, streamTaskHtml, uglifyCss, uglifyJs)//images
 
-function clean(cb) {
-  console.log(`clean时打印`)
-  cb()
-}
 
-function build(cb) {
-  console.log(`build时打印`)
-  console.log(process.env.NODE_ENV)
-  cb()
-}
+// ------------------------- 分割线 (以下为练习) -------------------------
 
-function promiseTask() {
-  return Promise.resolve('the value is ignored');
-}
-exports.promiseTask = promiseTask;
+// function defaultTask(cb) {
+//   console.log(`新版gulp4.0.0的语法`)
+//   cb();
+// }
 
-const { EventEmitter } = require('events');
-function eventEmitterTask() {
-  const emitter = new EventEmitter();
-  setTimeout(() => emitter.emit('finish'), 0);
-  return emitter;
-}
-exports.eventEmitterTask = eventEmitterTask;
+// function clean(cb) {
+//   console.log(`clean时打印`)
+//   cb()
+// }
 
-const { exec } = require('child_process');
-function childProcessTask() {
-  return exec('date');
-}
-exports.childProcessTask = childProcessTask;
+// function build(cb) {
+//   console.log(`build时打印`)
+//   console.log(process.env.NODE_ENV)
+//   cb()
+// }
 
-const { Observable } = require('rxjs');
-function observableTask() {
-  return Observable.of(1, 2, 3);
-}
-exports.observableTask = observableTask;
+// function promiseTask() {
+//   return Promise.resolve('the value is ignored');
+// }
+// exports.promiseTask = promiseTask;
 
-exports.clean = clean
-// exports.build = build
-// exports.default = defaultTask
-// exports.default = series(clean, build)
-// exports.default = parallel(clean, build)
-exports.build = series(clean, parallel(defaultTask, build), series(build, clean))
+// const { EventEmitter } = require('events');
+// function eventEmitterTask() {
+//   const emitter = new EventEmitter();
+//   setTimeout(() => emitter.emit('finish'), 0);
+//   return emitter;
+// }
+// exports.eventEmitterTask = eventEmitterTask;
+
+// const { exec } = require('child_process');
+// function childProcessTask() {
+//   return exec('date');
+// }
+// exports.childProcessTask = childProcessTask;
+
+// const { Observable } = require('rxjs');
+// function observableTask() {
+//   return Observable.of(1, 2, 3);
+// }
+// exports.observableTask = observableTask;
+
+// exports.clean = clean
+// // exports.build = build
+// // exports.default = defaultTask
+// // exports.default = series(clean, build)
+// // exports.default = parallel(clean, build)
+// exports.build = series(clean, parallel(defaultTask, build), series(build, clean))
